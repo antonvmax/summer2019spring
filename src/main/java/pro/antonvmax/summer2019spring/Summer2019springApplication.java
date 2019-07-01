207cf124dd0d3a26ae110596a2c68dd8d95d62be
Anton Maksimenkov <a.maksimenkov@intterra.ru>
2019-07-01 11:21:30 +0300

    Initial commit

---
+++ src/main/java/pro/antonvmax/summer2019spring/Summer2019springApplication.java
@@ -0,0 +1,13 @@
+package pro.antonvmax.summer2019spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summer2019springApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Summer2019springApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/antonvmax/summer2019spring/Summer2019springApplicationTests.java
@@ -0,0 +1,16 @@
+package pro.antonvmax.summer2019spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Summer2019springApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
